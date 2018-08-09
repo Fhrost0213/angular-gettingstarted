a981a0b8e5256f51ef131077f13ca4f4a5d31620
MARKIT\bryan.vieregge <bryan.vieregge@markit.com>
2018-08-09 15:13:33 -0500

    Session 8 - Building Nested Components - added a nested component and added input/output directives

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -65,4 +65,8 @@ export class ProductListComponent
         return this.products.filter((product: IProduct) => 
                 product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
 }
