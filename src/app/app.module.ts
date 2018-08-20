8c29185651d8e678434703638837bb35e698aad1
Fhrost0213 <bryan.vieregge@gmail.com>
2018-08-20 08:28:52 -0500

    Session 13 - Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { FormsModule } from '@angular/forms';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { RouterModule } from '@angular/router';
+import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule, 
-    FormsModule, 
-    RouterModule.forRoot(
-      [
-        { path: 'products', component: ProductListComponent },
-        { path: 'products/:id', component: ProductDetailComponent },
-        { path: 'welcome', component: WelcomeComponent },
-        { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-        { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-      ]
-    )
+    ProductModule,
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
